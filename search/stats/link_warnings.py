2e6051ba5eaff945c82a81b416756948d38adb7e
davidsaltacc <d.lumpi@outlook.de>
2025-08-18 21:00:12 +0200

    add nitroflare to slow filehosts

--- search/stats/link_warnings.py
+++ search/stats/link_warnings.py
@@ -3,10 +3,11 @@ from enum import Enum
 class NotRecommendedFilehost(Enum): # more can be added, there are just some more used ones
     DDownload = 1 # slow without premium
     RapidGator = 2 # slow without premium
-    OneFichier = 3 # annoying limitations
-    Qiwi = 4 # with their migration to ranoz.gg, qiwi links don't work 
-    ZippyShare = 5 # shut down
-    PixelDrain = 6 # is good in terms of speed, has a download limit though - can be bypassed
+    NitroFlare = 3 # slow without premium
+    OneFichier = 4 # annoying limitations
+    Qiwi = 5 # with their migration to ranoz.gg, qiwi links don't work 
+    ZippyShare = 6 # shut down
+    PixelDrain = 7 # is good in terms of speed, has a download limit though - can be bypassed
 
 def information_filehost(items):
     
@@ -17,6 +18,8 @@ def information_filehost(items):
             filehost = NotRecommendedFilehost.DDownload
         if "rapidgator" in item.lower():
             filehost = NotRecommendedFilehost.RapidGator
+        if "nitroflare" in item.lower():
+            filehost = NotRecommendedFilehost.NitroFlare
         if "1fichier" in item.lower():
             filehost = NotRecommendedFilehost.OneFichier
         if "qiwi" in item.lower():
@@ -31,6 +34,8 @@ def information_filehost(items):
             return "Warning: A DDownload link was detected. Their downloads are extemely slow without their premium plan, you might want to choose a different link if available."
         if filehost == NotRecommendedFilehost.RapidGator:
             return "Warning: A RapidGator link was detected. Their downloads are extemely slow without their premium plan, you might want to choose a different link if available."
+        if filehost == NotRecommendedFilehost.NitroFlare:
+            return "Warning: A NitroFlare link was detected. Their downloads are extemely slow without their premium plan, you might want to choose a different link if available."
         if filehost == NotRecommendedFilehost.OneFichier:
             return "Warning: A 1Fichier link was detected. Their download speed isn't extremely slow, but the site has several limitations on their free plan. For example, if you try to access a file a second time, you will have to wait out a very long countdown. You might want to choose a different link if available."
         if filehost == NotRecommendedFilehost.Qiwi:
